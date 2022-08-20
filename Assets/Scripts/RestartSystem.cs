b2d616e040a84127bf4d1522eb201ebfe9bf7651
Mattlo16 <73414121+Mattlo16@users.noreply.github.com>
2022-08-20 17:02:41 -0400

    Commentation
    Added necessary commentation to the restart system.

--- Assets/Scripts/RestartSystem.cs
+++ Assets/Scripts/RestartSystem.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine; // C# works with Unity.
+using UnityEngine.SceneManagement; // Needed to restart the scene.
 
 public class RestartSystem : MonoBehaviour
 {
@@ -14,13 +14,13 @@ public class RestartSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R)){
+        if (Input.GetKeyDown(KeyCode.R)){ //When I press "r" the game restarts.
             SceneRestart();
         }
     } 
 
-    public void SceneRestart()
+    public void SceneRestart() //The function I call to restart the scene.
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //This reloads the current scene that the game is on.
     }
 }
